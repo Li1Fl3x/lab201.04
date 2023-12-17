79fb3c193efabcbea932224feaa2dcd48bab8c41
Li1Fl3x <114523144+Li1Fl3x@users.noreply.github.com>
2023-12-17 16:53:07 +0200

    Добавьте файлы проекта.

---
+++ lab201.04/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace lab201._04
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnalyzeButtonClick(object sender, RoutedEventArgs e)
+        {
+            string inputString = inputTextBox.Text;
+            int dotCount = CountOccurrences(inputString, '.');
+            int commaCount = CountOccurrences(inputString, ',');
+
+            resultTextBlock.Text = $"Количество точек: {dotCount}\nКоличество запятых: {commaCount}";
+        }
+
+        private int CountOccurrences(string input, char target)
+        {
+            int count = 0;
+            foreach (char character in input)
+            {
+                if (character == target)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
+
